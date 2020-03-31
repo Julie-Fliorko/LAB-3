0dbc4bba5ae716c8d4dfc2269d348c6213f48d54
Julie-Fliorko <57948112+Julie-Fliorko@users.noreply.github.com>
2020-03-31 20:46:38 +0300

    fixed code added try-with-resourses constraction

--- main/java/tableware/writer/TablewareWriter.java
+++ main/java/tableware/writer/TablewareWriter.java
@@ -1,19 +1,24 @@
 package tableware.writer;
 
 import java.io.IOException;
+import java.io.StringWriter;
 import java.io.Writer;
 import java.util.List;
 
 import tableware.model.AbstractTableware;
 
 public class TablewareWriter {
-	private Writer csvWriter;
-	
+	private Writer csvWriter = new StringWriter();
+
+	public Writer getCsvWriter() {
+		return csvWriter;
+	}
+
 	public void setCsvWriter(Writer csvWriter) {
 		this.csvWriter = csvWriter;
 	}
-	
-	public void writerToFile(List<AbstractTableware> tableware) throws IOException  {
+
+	public void writerToFile(List<AbstractTableware> tableware) throws IOException {
 		csvWriter.write(tableware.get(0).getHeaders());
 		csvWriter.write("\n");
 		for (AbstractTableware goods : tableware) {
@@ -22,8 +27,4 @@ public class TablewareWriter {
 		}
 		csvWriter.flush();
 	}
-
-	@Override
-	public String toString() {
-		return csvWriter.toString();
-	}}
+}
